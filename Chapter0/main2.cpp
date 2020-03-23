6288b27a018b02b8354e58e1a2fbd9b5c734da9c
kkminseok <30401054+kkminseok@users.noreply.github.com>
2020-03-23 20:42:14 +0900

    Add files via upload

---
+++ Chapter0/main2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<<"Hello, World!"<<std::endl;
+
+    std::cout<<"KMS!"<<std::endl;
+
+    std::cout<<"Hello, World! 2"<<std::endl;
+
+    return 0;
+}
